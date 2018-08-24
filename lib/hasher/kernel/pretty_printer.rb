1725796bac99b1e96e848cac295b355f5fd204c2
Alexander <a.brjakin@avia-centr.ru>
2018-08-24 17:02:06 +0300

    Make PrettyPrinter be able to return an array of string, representing a tree structure, refs #feature/kernel_tree_creation

--- lib/hasher/kernel/pretty_printer.rb
+++ lib/hasher/kernel/pretty_printer.rb
@@ -1,10 +1,21 @@
 # frozen_string_literal: true
 
 module Kernel
+  # TODO: Configuration:
+  #
+
   class PrettyPrinter
+    attr_reader :result
+
+    def initialize
+      @result = []
+    end
+
     def pretty_print(tree)
       envelope do
-        pretty_print_recursive(tree)
+        reset_result!
+        build_result!(tree)
+        result.each { |str| puts str }
       end
     end
 
@@ -14,38 +25,24 @@ module Kernel
       5.times { puts }
     end
 
-    def pretty_print_recursive(current_node, base_output = '')
+    def build_result!(tree)
+      @result << tree.key
+      build_result_recursive!(tree)
+      @result
+    end
+
+    def reset_result!
+      @result = []
+    end
+
+    def build_result_recursive!(current_node, base_output = '')
       nodes_amount = current_node.nodes.count
       current_node.nodes.each_with_index do |node, index|
-        next print_leaf(base_output, node.key, node.nodes[0]) if node.leaf?
-        print_composite(base_output, node.key, nodes_amount, index) if node.composite?
-        pretty_print_recursive(node, format_base_output(base_output, nodes_amount, index))
+        next @result << get_leaf(base_output, node.key, node.nodes[0]) if node.leaf?
+        @result << get_composite(base_output, node.key, nodes_amount, index) if node.composite?
+        build_result_recursive!(node, format_base_output(base_output, nodes_amount, index))
       end
     end
-%{
-
-h = Hasher.new
-h.a.b.c.d.e = 5
-h.lat.lng.mandatory = 'coooool'
-h.figure.it.out = {a: 1}
-h.__pretty_print
-
-t = h.__tree
-
-__tree__h_a_b = t.nodes[0].nodes[0]
-__tree__h_lat = t.nodes[1]
-__tree__h_lat_lng = t.nodes[1].nodes[0]
-__tree__h_lat_lng.type = Kernel::TYPES::TREE::TYPE_COMPOSITE
-
-__tree__h_a_b.nodes << Kernel::Tree.new(type: Kernel::TYPES::TREE::TYPE_COMPOSITE, root: t.root, key: :dolor)
-__tree__h_lat.nodes << Kernel::Tree.new(type: Kernel::TYPES::TREE::TYPE_COMPOSITE, root: t.root, key: :sit)
-__tree__h_lat_lng.nodes << Kernel::Tree.new(type: Kernel::TYPES::TREE::TYPE_COMPOSITE, root: t.root, key: :amet)
-
-h.__pretty_print
-
-
-t.nodes << Kernel::Tree.new(type: Kernel::TYPES::TREE::TYPE_COMPOSITE, root: t.root, key: :lexus)
-}
 
     def format_base_output(base_output, nodes_amount, current_node_index)
       return "#{base_output}   " if nodes_amount == 1 || current_node_index == nodes_amount - 1
@@ -57,13 +54,13 @@ t.nodes << Kernel::Tree.new(type: Kernel::TYPES::TREE::TYPE_COMPOSITE, root: t.r
       current_node_index == nodes_amount - 1
     end
 
-    def print_composite(base_output, key, nodes_amount, current_node_index)
-      return puts "#{base_output}#{pattern_branch_ramificating_last}#{pattern_key(key)}" if last?(nodes_amount, current_node_index)
-      puts "#{base_output}#{pattern_branch_ramificating}#{pattern_key(key)}"
+    def get_composite(base_output, key, nodes_amount, current_node_index)
+      return "#{base_output}#{pattern_branch_ramificating_last}#{pattern_key(key)}" if last?(nodes_amount, current_node_index)
+      "#{base_output}#{pattern_branch_ramificating}#{pattern_key(key)}"
     end
 
-    def print_leaf(base_output, key, value)
-      puts "#{base_output}#{pattern_branch_ramificating_last}#{pattern_key(key)}#{pattern_value(value)}"
+    def get_leaf(base_output, key, value)
+      "#{base_output}#{pattern_branch_ramificating_last}#{pattern_key(key)}#{pattern_value(value)}"
     end
 
     def pattern_key(key)
@@ -96,19 +93,6 @@ t.nodes << Kernel::Tree.new(type: Kernel::TYPES::TREE::TYPE_COMPOSITE, root: t.r
 
     def pattern_composite_node(key)
       put key
-    end
-
-    def count_leafs(current_node)
-      count_leafs_recursive(current_node)
-    end
-
-    def count_leafs_recursive(current_node)
-      return 1 if current_node.leaf?
-      result = 0
-      current_node.nodes.each do |node|
-        result += count_leafs_recursive(node)
-      end
-      result
     end
   end
 end
