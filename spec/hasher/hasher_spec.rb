a6eb0c90725f5e312704baa3ed2419984d170080
Alexander <alexander@alexander.brjakin>
2018-11-28 08:35:04 +0300

    Write tests for `Hasher`, refs #tests.

--- spec/hasher/hasher_spec.rb
+++ spec/hasher/hasher_spec.rb
@@ -8,47 +8,107 @@ RSpec.describe Hasher do
   end
 
   describe '#method_missing' do
-    context 'when one link is specified in chain' do
-      it 'assigning' do
+    context 'shallow' do
+      it 'with simple values' do
         h = subject.new
-        h.lets = 5
-        expect(h.lets!).to eq(5)
+        h.a = 1
+        expect(h.a).to eq(1)
+        h.b = 2
+        expect(h.b).to eq(2)
+        expect(h.a).to eq(1)
+      end
+
+      it 'with array' do
+        h = subject.new
+        h.a = [1, 2]
+        expect(h.a).to eq([1, 2])
+        h.b = 2
+        expect(h.b).to eq(2)
+        expect(h.a).to eq([1, 2])
+      end
+
+      it 'with hash' do
+        h = subject.new
+        h.a = { b: 2 }
+        expect(h.a.b).to eq(2)
+        expect(h.a.c).to eq(nil)
+        expect(h.unknown).to eq(nil)
+      end
+
+      it 'with consts' do
+        h = subject.new
+        h.arr_class = Array
+        expect(h.arr_class).to eq(Array)
+        expect(h.nothing).to eq(nil)
       end
     end
 
-    context 'when many links are specified in chain' do
-      it 'assigning' do
+    context 'deep' do
+      it 'hash in array' do
         h = subject.new
-        h.lets.rock = 5
-        expect(h.lets.rock!).to eq(5)
+        h.a = 5
+        h.array = [1, { c: 2 }]
+        expect(h.a).to eq(5)
+        expect(h.array[0]).to eq(1)
+        expect(h.array[1].c).to eq(2)
+        expect(h.array[1].d).to eq(nil)
+      end
+
+      it 'array in hash' do
+        h = subject.new
+        h.a = { b: [1, 2] }
+        expect(h.a.b).to eq([1, 2])
+        expect(h.a.b[0]).to eq(1)
+        expect(h.a.b[1]).to eq(2)
+        expect(h.a.b[2]).to eq(nil)
+      end
+
+      it 'mixed' do
+        h = subject.new
+        h.a = 1
+        h.b = [2, { c: 3, d: 'str', e: [FalseClass, 5, { d: 6 }] }]
+        h.c = { left: { overs: true }, back: [75, { at: 84 }] }
+        h.c.false = false
+        h.c.true = true
+        h.array_class = Array
+        expect(h.a).to eq(1)
+        expect(h.b).to be_kind_of(Array)
+        expect(h.b[0]).to eq(2)
+        expect(h.b[1].c).to eq(3)
+        expect(h.b[1].d).to eq('str')
+        expect(h.b[1].e).to be_kind_of(Array)
+        expect(h.b[1].undefined_key).to eq(nil)
+        expect(h.b[1].e[0]).to eq(FalseClass)
+        expect(h.b[1].e[1]).to eq(5)
+        expect(h.b[1].e[2].d).to eq(6)
+        expect(h.b[1].e[2].unknown).to eq(nil)
+        expect(h.c.left.overs).to eq(true)
+        expect(h.c.back).to be_kind_of(Array)
+        expect(h.c.back.first).to eq(75)
+        expect(h.c.back[1].at).to eq(84)
+        expect(h.c.false).to eq(false)
+        expect(h.c.true).to eq(true)
+        expect(h.array_class).to eq(Array)
       end
     end
 
-    context 'when links was not specified' do
-      it 'returns nil' do
+    context 'reassignments' do
+      it 'mixed' do
         h = subject.new
-        expect(h.lets!).to eq(nil)
-        expect(h.lets.rock!).to eq(nil)
-        h.lets.rock = 5
-        expect(h.lets.rock.baby!).to eq(nil)
-      end
-    end
-
-    context 'when middle link is requested by exclamation' do
-      it 'returns Hasher' do
-        h = subject.new
-        h.lets.rock = 'Ohh yeah, baby!'
-        expect(h.lets).to be_an_instance_of(subject)
-      end
-    end
-
-    context 'when link has many nodes' do
-      it 'returns values according to requested node' do
-        h = subject.new
-        h.lets.rock = 5
-        h.lets.chill = 10
-        expect(h.lets.rock!).to eq(5)
-        expect(h.lets.chill!).to eq(10)
+        h.a = 1
+        expect(h.a).to eq(1)
+        h.b = [Numeric, 1, { c: [2, { d: 3 }] }]
+        expect(h.b[0]).to eq(Numeric)
+        expect(h.b[1]).to eq(1)
+        expect(h.b[2].c[0]).to eq(2)
+        expect(h.b[2].c[1].d).to eq(3)
+        h.b[2].c = false
+        expect(h.b[2].c).to eq(false)
+        expect(h.b[0]).to eq(Numeric)
+        expect(h.b[1]).to eq(1)
+        h.b = { deep: { inside: { our: { minds: { lies: 'awareness' } } } } }
+        expect(h.b.deep.inside.our.minds.lies).to eq('awareness')
+        expect(h.b.deep.mess).to eq(nil)
       end
     end
   end
